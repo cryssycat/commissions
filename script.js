d1c2005dc53f577ed18dfeb4772ec809bb1b701d
cryssycat <crysthigpen@gmail.com>
2026-06-29 07:30:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -48,6 +48,7 @@ async function loadCommissions() {
       const details = document.createElement("p");
       details.textContent = c.details || "";
 
+      const btn = document.createElement("button");
       content.appendChild(title);
       content.appendChild(price);
       content.appendChild(details);
